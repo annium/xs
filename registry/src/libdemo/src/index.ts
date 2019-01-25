09cf29f45453bc17e4ebf46687414007d8090a47
Alex Kreskiyan <a.kreskiyan@gmail.com>
2019-01-25 11:58:31 +0300

    Add separate lib

---
+++ registry/src/libdemo/src/index.ts
@@ -0,0 +1,3 @@
+export const demo = (message: string): void => {
+  console.log('demo:', message);
+}
--- registry/src/site/src/App.tsx
+++ registry/src/site/src/App.tsx
@@ -7,7 +7,13 @@ import logo from './logo.svg'
 import styles from './App.module.scss'
 
 
+import { demo } from 'libdemo';
+
 export default class App extends Component {
+  handleClick = () => {
+    demo('lib');
+  }
+
   render() {
     return (
       <div className={styles.container}>
@@ -15,6 +21,7 @@ export default class App extends Component {
           <img src={logo} className={styles.logo} alt="logo" />
           <p>Edit <code>src/App.tsx</code> and save to reload.</p>
           <a className={styles.link} href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
+          <button onClick={this.handleClick}>libdemo</button>
         </header>
       </div>
     );
