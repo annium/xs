18e5d713396d337fe2e74b9ad904ebef391e772f
Alex Kreskiyan <a.kreskiyan@gmail.com>
2019-01-26 21:45:51 +0300

    registry: fix site permissions

--- registry/src/site/src/App.tsx
+++ registry/src/site/src/App.tsx
@@ -7,22 +7,7 @@ import logo from './logo.svg'
 import styles from './App.module.scss'
 
 
-import * as lib from 'site.lib'
-
 export default class App extends Component {
-  private api: lib.api.Client
-
-  constructor(props: {}) {
-    super(props)
-    this.api = lib.api.factory({ url: 'http://localhost:9901' })
-  }
-
-  handleClick = async () => {
-    console.log('send request')
-    const regsitry = await this.api.get('registry');
-    console.log('regsitry info:', regsitry)
-  }
-
   render() {
     return (
       <div className={styles.container}>
@@ -30,7 +15,6 @@ export default class App extends Component {
           <img src={logo} className={styles.logo} alt="logo" />
           <p>Edit <code>src/App.tsx</code> and save to reload.</p>
           <a className={styles.link} href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
-          <button onClick={this.handleClick}>site.lib</button>
         </header>
       </div>
     );
