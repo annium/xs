f86a257f4e01a5f71e79ab01a5cddabdc3bf581f
Alex Kreskiyan <a.kreskiyan@gmail.com>
2019-01-27 14:54:40 +0300

    cli: fix clean command to allow mask

--- cli/src/Xs.Cli.Main/Commands/CleanCommand.cs
+++ cli/src/Xs.Cli.Main/Commands/CleanCommand.cs
@@ -9,7 +9,7 @@ using Xs.Cli.Main.Tools;
 
 namespace Xs.Cli.Main.Commands
 {
-    internal class CleanCommand : AsyncCommand<CwdCommandConfiguration>
+    internal class CleanCommand : AsyncCommand<CleanCommandConfiguration, CwdCommandConfiguration>
     {
         public override string Id { get; } = "clean";
 
@@ -17,31 +17,43 @@ namespace Xs.Cli.Main.Commands
 
         private readonly DiscoverProjectsTask discoverTask;
 
+        private readonly FilterProjectsTask filterTask;
+
         private readonly ProjectsRunner runner;
 
         private readonly ILogger logger;
 
         public CleanCommand(
             DiscoverProjectsTask discoverTask,
+            FilterProjectsTask filterTask,
             ProjectsRunner runner,
             ILogger logger
         )
         {
             this.discoverTask = discoverTask;
+            this.filterTask = filterTask;
             this.runner = runner;
             this.logger = logger;
         }
 
         public override async Task HandleAsync(
+            CleanCommandConfiguration cfg,
             CwdCommandConfiguration cwdCfg,
             CancellationToken token
         )
         {
-            var projects = (await discoverTask.RunAsync(cwdCfg.Cwd))
+            var projects = filterTask.Run(await discoverTask.RunAsync(cwdCfg.Cwd), cfg.Mask)
                 .OfType<ICleanableProject>()
                 .ToArray();
             logger.LogDebug($"Clean {projects.Length} projects.");
             await runner.RunAsync(projects, (project, tkn) => project.CleanAsync(tkn), token);
         }
+    }
+
+    internal class CleanCommandConfiguration
+    {
+        [Position(1, isRequired : false)]
+        [Help("Projects mask.")]
+        public string Mask { get; set; } = "all";
     }
 }
