3b08db62fb02b69435f229d0554d2b70e446e33f
Alex Kreskiyan <a.kreskiyan@gmail.com>
2019-02-05 22:12:56 +0300

    registry: extend IContext to operate on entities

--- registry/src/Xs.Registry.Db/IContext.cs
+++ registry/src/Xs.Registry.Db/IContext.cs
@@ -1,10 +1,13 @@
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace Xs.Registry.Db
 {
     internal interface IContext
     {
+        EntityEntry<TEntity> Entry<TEntity>(TEntity entity) where TEntity : class;
+
         Task<int> SaveChangesAsync(CancellationToken cancellationToken = default(CancellationToken));
     }
 }
