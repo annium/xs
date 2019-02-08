017a2da310ef5016ae410eff29aec19a18caf4e2
Alex Kreskiyan <a.kreskiyan@gmail.com>
2019-02-08 16:10:33 +0300

    registry: wire up views

--- registry/src/Xs.Registry.Node/Configuration.cs
+++ registry/src/Xs.Registry.Node/Configuration.cs
@@ -6,6 +6,8 @@ namespace Xs.Registry.Node
 {
     internal class Configuration : IConfiguration
     {
+        public const string DateFormat = "yyyy-MM-ddTHH:mm:ss.fffK";
+
         public readonly string PackagesFolder = Path.Combine("data", Constants.ProjectType.ToString(), "packages");
 
         public Uri Location { get; set; }
