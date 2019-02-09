70026564aa34ce1998d8cf093e139c4c07437c88
Alex Kreskiyan <a.kreskiyan@gmail.com>
2019-02-09 19:53:22 +0300

    registry site: basic main page with search input

--- registry-site/src/site/src/pages/MainPage/index.tsx
+++ registry-site/src/site/src/pages/MainPage/index.tsx
@@ -1,14 +1,44 @@
+import Col, { ColSize } from 'antd/lib/col'
+import Input from 'antd/lib/input'
+import Row from 'antd/lib/row'
+import { observable } from 'mobx'
+import { observer } from 'mobx-react'
 import * as React from 'react'
+
+import styles from './index.module.scss'
 
 
 const log = console.log.bind(console, 'MainPage')
+@observer
 export default class MainPage extends React.Component {
+  @observable private query: string = ''
+
   render() {
     log('render')
+    const { query } = this
+
     return (
-      <div className="main-page">
-        Main page
+      <div className={styles.page}>
+        <Row>
+          <Col {...this.getLayout()}>
+            <h1>Main page</h1>
+            <Input.Search placeholder="search packages" enterButton onSearch={this.setQuery} />
+            <h2>{query}</h2>
+          </Col>
+        </Row>
       </div>
     )
   }
+
+  private getLayout(): { [key: string]: ColSize } {
+    return {
+      xs: { offset: 1, span: 22 },
+      sm: { offset: 1, span: 22 },
+      md: { offset: 2, span: 20 },
+      lg: { offset: 3, span: 18 },
+      xl: { offset: 5, span: 14 },
+    }
+  }
+
+  private setQuery = (value: string) => this.query = value
 }
