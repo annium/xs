87f20d4c5fb48f74b1e155b323a70f1cbe9f3c1e
Alex Kreskiyan <a.kreskiyan@gmail.com>
2019-02-10 15:43:10 +0300

    registry: implement UserMetaPackageAccess.None

--- registry/src/Xs.Registry.Db/Shared/Models/UserMetaPackageAccess.cs
+++ registry/src/Xs.Registry.Db/Shared/Models/UserMetaPackageAccess.cs
@@ -6,6 +6,9 @@ namespace Xs.Registry.Db.Shared
 {
     public struct UserMetaPackageAccess
     {
+        public static readonly UserMetaPackageAccess None =
+            new UserMetaPackageAccess(Guid.NewGuid(), Guid.NewGuid(), Array.Empty<MetaPackagePermission>());
+
         public bool IsOwner { get; }
 
         public bool IsWorld { get; }
@@ -27,5 +30,25 @@ namespace Xs.Registry.Db.Shared
         }
 
         public bool Has(Permission permission) => this.permission.HasFlag(permission);
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hash = 7;
+
+                hash = hash * 31 + IsOwner.GetHashCode();
+                hash = hash * 31 + IsWorld.GetHashCode();
+                hash = hash * 31 + permission.GetHashCode();
+
+                return hash;
+            }
+        }
+
+        public override bool Equals(object obj) => GetType() == obj?.GetType() && GetHashCode() == obj.GetHashCode();
+
+        public static bool operator ==(UserMetaPackageAccess a, UserMetaPackageAccess b) => a.GetHashCode() == b.GetHashCode();
+
+        public static bool operator !=(UserMetaPackageAccess a, UserMetaPackageAccess b) => !(a == b);
     }
 }
