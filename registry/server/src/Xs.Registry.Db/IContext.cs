855a5fb8dbad2dd130d0f059882f7ed9e58067c6
Alex Kreskiyan <a.kreskiyan@gmail.com>
2019-02-15 14:57:51 +0300

    registry: remove useless context methods

--- registry/server/src/Xs.Registry.Db/IContext.cs
+++ registry/server/src/Xs.Registry.Db/IContext.cs
@@ -1,16 +1,9 @@
-using System.Threading;
-using System.Threading.Tasks;
 using LinqToDB.Data;
-using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace Xs.Registry.Db
 {
     internal interface IContext
     {
         DataConnection GetDataConnection();
-
-        EntityEntry<TEntity> Entry<TEntity>(TEntity entity) where TEntity : class;
-
-        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default(CancellationToken));
     }
 }
