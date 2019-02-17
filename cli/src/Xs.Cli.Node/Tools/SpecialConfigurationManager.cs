a53ad1b0a0dbd64e3b4c7237e1abd0a39fa01f2a
Alex Kreskiyan <a.kreskiyan@gmail.com>
2019-02-17 20:51:07 +0300

    cli: for node SpecialConfigurationManager - save all used scopes except reserved to .npmrc

--- cli/src/Xs.Cli.Node/Tools/SpecialConfigurationManager.cs
+++ cli/src/Xs.Cli.Node/Tools/SpecialConfigurationManager.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 using Xs.Cli.Core.Models;
 using Xs.Cli.Core.Projects;
@@ -9,23 +11,31 @@ namespace Xs.Cli.Node.Tools
 {
     internal class SpecialConfigurationManager : ISpecialConfigurationManager
     {
+        private const string file = ".npmrc";
+
+        private static readonly IEnumerable<string> reservedScopes = new [] { "types" };
+
         public ProjectType Type { get; } = Constants.ProjectType;
 
         public string IgnorePattern { get; } = file;
 
-        private const string file = ".npmrc";
-
         public void Save(IProject project, Uri location, string token)
         {
             // with NPM currently it's not possible to publish unscoped packages privately
-            if (!project.Name.StartsWith('@'))
+            var scope = GetScope(project.Name);
+            if (scope == null)
                 return;
 
-            var scope = project.Name.Substring(1).Split('/') [0];
             var sb = new StringBuilder();
             sb.AppendLine($"@{scope}:registry={location}");
+            // add all used scopes except reserved
+            foreach (var dependencyScope in project.PackageDependencies.Select(d => GetScope(d.Name)).OfType<string>())
+                if (!reservedScopes.Contains(dependencyScope))
+                    sb.AppendLine($"@{dependencyScope}:registry={location}");
             sb.AppendLine($"//{location.Authority}/:_authToken=\"{token}\"");
             File.WriteAllText(FilePath(project), sb.ToString());
+
+            string GetScope(string name) => name.StartsWith('@') ? null : name.Substring(1).Split('/') [0];
         }
 
         public void Delete(IProject project)
