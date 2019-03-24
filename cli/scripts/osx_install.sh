9735e5e9154daac2f1e3abfb9ad1fe3a6bfe7b4b
Alex Kreskiyan <a.kreskiyan@gmail.com>
2019-03-24 16:10:25 +0300

    Add simple launcher

--- cli/scripts/osx_install.sh
+++ cli/scripts/osx_install.sh
@@ -23,3 +23,11 @@ rm -f $relaxed
 echo '#!/usr/bin/env sh' > $relaxed
 echo $root'/Xs.Cli.Main $@ --skip-checks' >> $relaxed
 chmod +x $relaxed
+
+# prepare simple launcher
+simple=/usr/local/bin/xss
+echo "Write simple launcher."
+rm -f $simple
+echo '#!/usr/bin/env sh' > $simple
+echo $root'/Xs.Cli.Main $@ --ignore-consistency --skip-checks' >> $simple
+chmod +x $simple
