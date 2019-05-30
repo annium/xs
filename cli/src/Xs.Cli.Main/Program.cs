875e78500df6c53f78d2a723a8c8709dc061c0b9
Alex Kreskiyan <a.kreskiyan@gmail.com>
2019-05-30 11:26:09 +0300

    Add --verbose toggle

--- cli/src/Xs.Cli.Main/Program.cs
+++ cli/src/Xs.Cli.Main/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Threading;
 using Annium.Extensions.Arguments;
 using Annium.Extensions.Entrypoint;
@@ -13,7 +14,43 @@ namespace Xs.Cli.Main
             CancellationToken token
         )
         {
-            new Commander(provider).Run<Commands.Group>(args, token);
+            var verbose = args.Contains("--verbose");
+
+            try
+            {
+                new Commander(provider).Run<Commands.Group>(args, token);
+            }
+            catch (AggregateException exception)
+            {
+                LogAggregateException(exception, verbose);
+            }
+            catch (Exception exception)
+            {
+                LogException(exception, verbose);
+            }
+        }
+
+        private static void LogAggregateException(AggregateException aggregateException, bool verbose)
+        {
+            var color = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            var exceptions = aggregateException.Flatten().InnerExceptions;
+            Console.WriteLine($"Errors ({exceptions.Count}):");
+            foreach (var exception in exceptions)
+                Console.WriteLine(verbose ? exception.ToString() : exception.Message);
+
+            Console.ForegroundColor = color;
+        }
+
+        private static void LogException(Exception exception, bool verbose)
+        {
+            var color = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            Console.WriteLine(verbose ? exception.ToString() : exception.Message);
+
+            Console.ForegroundColor = color;
         }
 
         public static int Main(string[] args) => new Entrypoint()
