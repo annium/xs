e090f10ee36ca8a8d28da6f46190c122a1703144
Alex Kreskiyan <a.kreskiyan@gmail.com>
2019-05-30 06:58:15 +0300

    finished win script

--- cli/scripts/win_install.ps1
+++ cli/scripts/win_install.ps1
@@ -1,26 +1,27 @@
 $dir = (Get-Item $PSScriptRoot).Parent.FullName
 
-$root = Join-Path $HOME Projects lib xs
-New-Item -Path (Join-Path $HOME Projects) -Name bin -ItemType Directory -Force | Out-Null
+$root = Join-Path $HOME Documents Projects lib xs
+New-Item -Path (Join-Path $HOME Documents Projects) -Name bin -ItemType Directory -Force | Out-Null
 
 Write-Output "Compile."
 if (Test-Path $root) { Remove-Item -Recurse -Force $root }
 dotnet publish -c release -r win-x64 -o $root $dir/src/Xs.Cli.Main/
 
+$nl = [System.Environment]::NewLine
 # prepare launcher
-$entry = Join-Path $HOME Projects bin xs.bat
+$entry = Join-Path $HOME Documents Projects bin xs.bat
 Write-Output "Write launcher."
 if (Test-Path $entry) { Remove-Item -Recurse -Force $entry }
-Write-Output "run $root/Xs.Cli.Main $@" > $entry
+Set-Content -Path $entry -Value "@echo off $nl $root\Xs.Cli.Main.exe %*"
 
 # prepare relaxed launcher
-$relaxed = Join-Path $HOME Projects bin ass.bat
+$relaxed = Join-Path $HOME Documents Projects bin ass.bat
 Write-Output "Write relaxed launcher."
 if (Test-Path $relaxed) { Remove-Item -Recurse -Force $relaxed }
-Write-Output $root'/Xs.Cli.Main $@ --skip-checks' > $relaxed
+Set-Content -Path $relaxed -Value "@echo off $nl $root\Xs.Cli.Main.exe %* --skip-checks"
 
 # prepare simple launcher
-$simple = Join-Path $HOME Projects bin xss.bat
+$simple = Join-Path $HOME Documents Projects bin xss.bat
 Write-Output "Write simple launcher."
 if (Test-Path $simple) { Remove-Item -Recurse -Force $simple }
-Write-Output $root'/Xs.Cli.Main $@ --ignore-consistency --skip-checks' > $simple
+Set-Content -Path $simple -Value "@echo off $nl $root\Xs.Cli.Main.exe %* --skip-checks --ignore-consistency"
