c843109568633930b93d99445ed1b10abfb9b74f
Alex Kreskiyan <a.kreskiyan@gmail.com>
2020-07-27 19:19:55 +0300

    cli: add basic wasm template

---
+++ cli/src/Xs.Cli.Dotnet/Commands/New/WebAssemblyCommand.cs
@@ -0,0 +1,57 @@
+using System.IO;
+using System.Threading;
+using Annium.Extensions.Arguments;
+using Annium.Logging.Abstractions;
+using Xs.Cli.Core.Commands;
+using Xs.Cli.Core.Tools;
+using Xs.Cli.Dotnet.Projects;
+
+namespace Xs.Cli.Dotnet.Commands.New
+{
+    public class WebAssemblyCommand : Command<WebAssemblyCommandConfiguration, DiscoverConfiguration>
+    {
+        public override string Id { get; } = "wasm";
+        public override string Description { get; } = "Create new WebAssembly project.";
+        private readonly ITemplateWriter _templateWriter;
+        private readonly ILogger<WebAssemblyCommand> _logger;
+
+        public WebAssemblyCommand(
+            ITemplateWriter templateWriter,
+            ILogger<WebAssemblyCommand> logger
+        )
+        {
+            _templateWriter = templateWriter;
+            _logger = logger;
+        }
+
+        public override void Handle(
+            WebAssemblyCommandConfiguration cfg,
+            DiscoverConfiguration discoverCfg,
+            CancellationToken token
+        )
+        {
+            var location = discoverCfg.Root;
+            var name = cfg.Name;
+
+            _logger.Debug($"Create WebAssembly project {name} at {location}");
+
+            _templateWriter.LoadResources($"{Group.TemplatesDir}.WebAssembly");
+            _templateWriter.SetRoot(Path.Combine(location, name));
+
+            // setup data
+            var data = new { name };
+
+            // write files
+            _templateWriter.Write(Group.ProjectTemplate, $"{name}{ProjectFactory.ProjectFileExtension}", data);
+            _templateWriter.WriteAll(data);
+            _templateWriter.EnsureAllWritten();
+        }
+    }
+
+    public class WebAssemblyCommandConfiguration
+    {
+        [Position(1)]
+        [Help("Project name.")]
+        public string Name { get; set; } = string.Empty;
+    }
+}
