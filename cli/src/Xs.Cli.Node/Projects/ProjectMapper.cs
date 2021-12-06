2733d3a4038122cf9ca3615c3c2c5f0c9492e5cf
Alex Kreskiyan <a.kreskiyan@gmail.com>
2021-12-07 01:31:38 +0300

    cli: fix node ProjetMapper

--- cli/src/Xs.Cli.Node/Projects/ProjectMapper.cs
+++ cli/src/Xs.Cli.Node/Projects/ProjectMapper.cs
@@ -20,7 +20,7 @@ internal class ProjectMapper : IProjectMapper<ISpecialProject, RawProject>
         Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
         DictionaryKeyPolicy = JsonNamingPolicy.CamelCase,
         WriteIndented = true,
-        IgnoreNullValues = true,
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
     };
 
     private const string FilePrefix = "file:";
