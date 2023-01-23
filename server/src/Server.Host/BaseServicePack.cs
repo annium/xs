9ed63d1b6a0b4dbd9d4e269274dec2fb39375736
Alex Kreskiyan <a.kreskiyan@gmail.com>
2023-01-23 22:18:22 +0300

    swagger: setup auth

--- server/src/Server.Host/BaseServicePack.cs
+++ server/src/Server.Host/BaseServicePack.cs
@@ -57,7 +57,31 @@ internal class BaseServicePack : ServicePackBase
 
     private void SetupSwagger(SwaggerGenOptions options)
     {
-        options.SwaggerDoc("v1", new OpenApiInfo { Version = "v1", Title = "Package server", });
+        options.SwaggerDoc("v1", new OpenApiInfo { Title = "Package server", Version = "v1", });
+        options.AddSecurityDefinition("token",
+            new OpenApiSecurityScheme
+            {
+                In = ParameterLocation.Header,
+                Description = "Please enter a valid token",
+                Name = "Authorization",
+                Type = SecuritySchemeType.Http,
+                BearerFormat = "JWT",
+                Scheme = "Bearer"
+            });
+        options.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                },
+                new string[] { }
+            }
+        });
     }
 
     public override void Setup(IServiceProvider provider)
