c312a93e32ae8a72fe5d4074063df6a1f312bafc
Alex Kreskiyan <a.kreskiyan@gmail.com>
2023-01-24 13:28:33 +0300

    server: fix meta_package_permissions pk

--- server/src/Server.Shared/Scripts/Migrations/0003_meta_package_permissions.sql
+++ server/src/Server.Shared/Scripts/Migrations/0003_meta_package_permissions.sql
@@ -2,6 +2,6 @@ create table main.meta_package_permissions (
 	meta_package_id uuid not null,
 	category int not null,
 	permission int not null,
-	constraint pk_meta_package_permissions primary key (meta_package_id),
+	constraint pk_meta_package_permissions primary key (meta_package_id, category, permission),
 	constraint fk_meta_package_permissions_meta_packages_meta_package_id foreign key (meta_package_id) references main.meta_packages(id) on delete restrict
 );
