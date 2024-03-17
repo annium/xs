c8ce677fe17c416bbf2ef9d6fe5fa29b22b3e718
Alex Kreskiyan <a.kreskiyan@gmail.com>
2024-03-17 16:41:10 +0300

    cli: upgrade ListInsCommand

--- cli/src/Xs/Commands/Ls/ListInsCommand.cs
+++ cli/src/Xs/Commands/Ls/ListInsCommand.cs
@@ -1,10 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Annium;
 using Annium.Extensions.Arguments;
+using Annium.Linq;
 using Xs.Cli.Core.Commands;
 using Xs.Cli.Core.Models;
 using Xs.Cli.Core.Projects;
@@ -36,102 +37,145 @@ internal class ListInsCommand : AsyncCommand<ListInsCommandConfiguration, Discov
         var showProjects = cfg.Projects || !cfg.Packages;
         var showPackages = cfg.Packages || !cfg.Projects;
 
+        // show hierarchical projects list
+        if (!cfg.Plain || cfg.Depth > 0 && cfg.Depth != int.MaxValue)
+        {
+            ShowProjectsTree(cfg, projects, showPackages);
+            return;
+        }
+
         // if plain/direct dependencies list requested - join deps and log them in single list
-        if (cfg.Plain || cfg.Depth == 0)
-        {
-            LogPlainDependencies(projects, showProjects, showPackages, cfg);
-            return;
-        }
-
-        // otherwise - log nice dependencies tree for given depth level
-        var nest = cfg.Depth;
-        var last = projects.Last();
-        foreach (var project in projects)
-            LogProjectWithDependencies(
-                new Dependency<IProject>(DependencyType.Normal, project),
-                showProjects,
-                showPackages,
-                string.Empty,
-                nest,
-                project == last
-            );
-    }
-
-    private void LogPlainDependencies(
-        IReadOnlyCollection<IProject> projects,
-        bool showProjects,
-        bool showPackages,
-        ListInsCommandConfiguration cfg
-    )
-    {
         if (showProjects)
-        {
-            var projectDeps = projects
-                .SelectMany(p => p.Projects)
-                .Select(d => d.Value)
-                .Distinct()
-                .OrderBy(e => e.Name)
-                .ToArray();
-            foreach (var dependency in projectDeps)
-                Console.WriteLine(dependency.Describe(cfg.Path, cfg.Attributes));
-        }
-
-        if (showPackages)
-        {
-            var packageDeps = projects
-                .SelectMany(p => p.Packages)
-                .Select(d => d.Value)
-                .Distinct()
-                .OrderBy(e => e.Name)
-                .ToArray();
-            foreach (var dependency in packageDeps)
-                Console.WriteLine(dependency);
-        }
+            // if plain/direct dependencies list requested - join deps and log them in single list
+            ShowPlainProjects(cfg, projects);
+        else if (showPackages)
+            ShowPlainPackages(cfg, projects);
     }
 
-    private void LogProjectWithDependencies(
-        Dependency<IProject> projectDependency,
-        bool showProjects,
-        bool showPackages,
-        string prefix,
-        int nest,
-        bool isLast
+    private void ShowProjectsTree(
+        ListInsCommandConfiguration cfg,
+        IReadOnlyCollection<IProject> projects,
+        bool showPackages
     )
     {
-        var (dependencyType, project) = projectDependency;
-        var packageDeps = project.Packages.OrderBy(e => e.Type).ThenBy(e => e.Value.Name).ToArray();
-        var projectDeps = project.Projects.OrderBy(e => e.Type).ThenBy(e => e.Value.Name).ToArray();
-        var node = isLast ? "└─" : "├─";
+        // select projects, not references by other projects
+        projects = projects.Where(x => projects.None(p => p.Projects.Any(d => d.Value == x))).ToArray();
 
-        var depsCount = (showProjects ? projectDeps.Length : 0) + (showPackages ? packageDeps.Length : 0);
-        if (depsCount == 0 || nest == 0)
+        var last = projects.Last();
+        var ctx = new PrefixedContext(cfg.Depth, showPackages);
+        foreach (var project in projects)
         {
-            Console.WriteLine($"{prefix}{node}─ {project} {project.Version} ({dependencyType})");
-            return;
+            var proj = new Dependency<IProject>(DependencyType.Normal, project);
+            Traverse(proj, cfg.Depth, 0, project == last, ctx, ShowProjectNode);
         }
 
-        Console.WriteLine($"{prefix}{node}┬ {project} {project.Version} ({dependencyType})");
-        prefix += isLast ? "  " : "│ ";
-
-        if (showPackages && packageDeps.Length > 0)
+        static void ShowProjectNode(Dependency<IProject> proj, int depth, bool isLast, PrefixedContext ctx)
         {
-            var last = projectDeps.Length > 0 ? null : packageDeps.Last();
-            foreach (var dependency in packageDeps)
-                LogPackage(dependency, prefix, !(last is null) && dependency == last);
-        }
+            var (dependencyType, project) = proj;
+            var packageDeps = project.Packages.OrderBy(e => e.Type).ThenBy(e => e.Value.Name).ToArray();
+            var projectDeps = project.Projects.OrderBy(e => e.Type).ThenBy(e => e.Value.Name).ToArray();
 
-        if (showProjects && projectDeps.Length > 0)
-        {
-            var last = projectDeps.Last();
-            foreach (var dependency in projectDeps)
-                LogProjectWithDependencies(dependency, true, showPackages, prefix, nest - 1, dependency == last);
+            {
+                var node = isLast ? "└─" : "├─";
+                var depsCount = projectDeps.Length + (ctx.ShowPackages ? packageDeps.Length : 0);
+                if (depsCount == 0 || ctx.Depth == depth)
+                {
+                    Console.WriteLine($"{ctx.Prefix}{node}─ {project} {project.Version} ({dependencyType})");
+                    return;
+                }
+
+                Console.WriteLine($"{ctx.Prefix}{node}┬ {project} {project.Version} ({dependencyType})");
+            }
+
+            ctx.ExtendPrefix(isLast ? "  " : "│ ");
+            if (ctx.ShowPackages && packageDeps.Length > 0)
+            {
+                var last = projectDeps.Length > 0 ? null : packageDeps.Last();
+                foreach (var dependency in packageDeps)
+                {
+                    var node = last is not null && dependency == last ? "└─" : "├─";
+                    Console.WriteLine($"{ctx.Prefix}{node}─ {dependency.Value} ({dependency.Type})");
+                }
+            }
         }
     }
 
-    private void LogPackage(Dependency<Package> package, string prefix, bool isLast)
+    private void ShowPlainProjects(ListInsCommandConfiguration cfg, IProject[] projects)
     {
-        var node = isLast ? "└─" : "├─";
-        Console.WriteLine($"{prefix}{node}─ {package} ({package.Type})");
+        var ctx = new CollectContext<IProject>();
+        foreach (var project in projects)
+        {
+            var proj = new Dependency<IProject>(DependencyType.Normal, project);
+            Traverse(proj, cfg.Depth, 0, false, ctx, CollectProjects);
+        }
+        foreach (var dependency in ctx.Items.Except(projects).OrderBy(x => x.Name))
+            Console.WriteLine(dependency.Describe(cfg.Path, cfg.Attributes));
+
+        static void CollectProjects(Dependency<IProject> project, int depth, bool isLast, CollectContext<IProject> ctx)
+        {
+            ctx.Items.Add(project.Value);
+        }
+    }
+
+    private void ShowPlainPackages(ListInsCommandConfiguration cfg, IProject[] projects)
+    {
+        var ctx = new CollectContext<Package>();
+        foreach (var project in projects)
+        {
+            var proj = new Dependency<IProject>(DependencyType.Normal, project);
+            Traverse(proj, cfg.Depth, 0, false, ctx, CollectPackages);
+        }
+        foreach (var dependency in ctx.Items.OrderBy(x => x.Name))
+            Console.WriteLine(dependency);
+
+        static void CollectPackages(Dependency<IProject> project, int depth, bool isLast, CollectContext<Package> ctx)
+        {
+            foreach (var dependency in project.Value.Packages)
+                ctx.Items.Add(dependency.Value);
+        }
+    }
+
+    private record CollectContext<T> : ICopyable<CollectContext<T>>
+    {
+        public HashSet<T> Items { get; } = new();
+
+        public CollectContext<T> Copy() => this;
+    }
+
+    private record PrefixedContext(int Depth, bool ShowPackages) : ICopyable<PrefixedContext>
+    {
+        public string Prefix { get; private set; } = string.Empty;
+
+        public void ExtendPrefix(string prefix)
+        {
+            Prefix = $"{Prefix}{prefix}";
+        }
+
+        public PrefixedContext Copy() => this with { };
+    }
+
+    private void Traverse<T>(
+        Dependency<IProject> projectDependency,
+        int maxDepth,
+        int depth,
+        bool isLast,
+        T ctx,
+        Action<Dependency<IProject>, int, bool, T> handle
+    )
+        where T : ICopyable<T>
+    {
+        ctx = ctx.Copy();
+        handle(projectDependency, depth, isLast, ctx);
+        if (depth == maxDepth)
+            return;
+
+        var projectDeps = projectDependency.Value.Projects.OrderBy(e => e.Type).ThenBy(e => e.Value.Name).ToArray();
+        if (projectDeps.Length <= 0)
+            return;
+
+        var last = projectDeps.Last();
+        foreach (var dependency in projectDeps)
+            Traverse(dependency, maxDepth, depth + 1, dependency == last, ctx, handle);
     }
 }
 
